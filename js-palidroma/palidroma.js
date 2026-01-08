84f11c996ce1855d7b54fc89ab7df1af981b9f16
micheleferri01 <micheleferri095@gmail.com>
2026-01-08 16:43:34 +0100

    aggiunto jsdoc comment per la funzione

--- js-palidroma/palidroma.js
+++ js-palidroma/palidroma.js
@@ -11,17 +11,22 @@ let wordIsPalindroma = isWordPalindroma(word);
 console.log(wordIsPalindroma);
 
 // messaggio per l'utente
-const message = wordIsPalindroma? 'La parola inserita è palindroma.': 'La parola inserita non è palidroma.';
+const message = wordIsPalindroma ? 'La parola inserita è palindroma.' : 'La parola inserita non è palidroma.';
 alert(message);
 
 
 // Dichiaro la funzione per controllare se la parola inserita è palidroma
+/**
+ * Questa funzione controlla se text è una parola palidroma
+ * @param { string } text stringa da controllare
+ * @returns { boolean } ritorna 'true' se la stringa è palindroma
+*/
 function isWordPalindroma(text) {
     // dichiaro una variabile dove salvare la parola invertita
     let reversedWord = "";
-    
+
     // inverto la parola fornita e la salvo
-    for(let i = 0; i < text.length; i++){
+    for (let i = 0; i < text.length; i++) {
         const currentChar = text[i];
         reversedWord = currentChar + reversedWord;
     }

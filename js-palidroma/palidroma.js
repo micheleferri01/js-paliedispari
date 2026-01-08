8af91679819820eb9b9cd7f7414eab63f3078a73
micheleferri01 <micheleferri095@gmail.com>
2026-01-08 15:50:18 +0100

    definisco il blocco di codice per vericare se la parola è palindroma nella sua apposita funzione

--- js-palidroma/palidroma.js
+++ js-palidroma/palidroma.js
@@ -3,10 +3,25 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // chiedo una parola all'utente
-const word = prompt('Inserire una parola', 'alla');
+const word = prompt('Inserire una parola', 'alla').trim().toLowerCase();
+console.log(word);
+
 
 
 // Dichiaro la funzione per controllare se la parola inserita è palidroma
 function isWordPalindroma(text) {
+    // dichiaro una variabile dove salvare la parola invertita
+    let reversedWord = "";
+    
+    // inverto la parola fornita e la salvo nella variabile
+    for(let i = 0; i < text.length; i++){
+        const currentChar = text[i];
+        reversedWord = currentChar + reversedWord;
+    }
 
+    // controllo se la parola è palindroma
+    const wordPalindroma = text === reversedWord;
+
+    // restituisco il risultato
+    return wordPalindroma;
 }

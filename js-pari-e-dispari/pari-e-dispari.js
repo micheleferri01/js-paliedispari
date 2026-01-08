16142836f3dfb83d5b2f6cb7f59c06c21b20adc6
micheleferri01 <micheleferri095@gmail.com>
2026-01-08 17:15:55 +0100

    creata finzione per generare numero randomico e generato numero randomico per pc

--- js-pari-e-dispari/pari-e-dispari.js
+++ js-pari-e-dispari/pari-e-dispari.js
@@ -9,6 +9,27 @@
 
 // chiedo all'utente 'pari o dispari?'
 const userChoice = 'pari' || prompt('pari o dispari', 'pari').trim().toLocaleLowerCase();
+console.log(userChoice);
 
 // chiedo all'utente un numero tra 1 e 5
-const userNumber = parseint(prompt('inserire un numero da 1 a 5', 1));
+const userNumber = 1 || parseint(prompt('inserire un numero da 1 a 5', 1));
+console.log(userNumber);
+
+
+// valori di range per la generazione del numero per il pc
+const max = 5;
+const min = 1;
+const pcNumber = generateRandomNumber(max,min);
+console.log(pcNumber);
+
+// #funzioni
+/**
+ * Questa funzione genera un numero randomico compreso nel range specificato
+ * @param {number} num1 valore massimo che può essere generato
+ * @param {number} num2 valore minimo che può essere generato
+ * @returns {number} ritorna un numero randomico compreso nel range
+ */
+function generateRandomNumber(num1,num2){
+    const randomNumber = Math.floor(Math.random() * (num1 - num2 + 1) + num2);
+    return randomNumber
+}

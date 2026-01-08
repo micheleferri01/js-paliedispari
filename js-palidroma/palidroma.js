802e76e11a588f64fcf821ef01d8950fe4f0d700
micheleferri01 <micheleferri095@gmail.com>
2026-01-08 15:23:23 +0100

    prompt per chiedere la parola

---
+++ js-palidroma/palidroma.js
@@ -0,0 +1,8 @@
+// #Richiesta
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedo una parola all'utente
+const word = prompt('Inserire una parola', 'alla');
+
+
